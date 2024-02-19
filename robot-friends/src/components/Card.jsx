4a6f10548df33fcb2ca0e26a7558c7894af0ffb9
oviera <oviera@vmware.com>
2024-02-19 15:51:15 +0200

    Change Color

--- robot-friends/src/components/Card.jsx
+++ robot-friends/src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Card extends React.Component {
     render() {
         return (
-            <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow5">
+            <div className="tc bg-light-purple dib br3 pa3 ma2 grow bw2 shadow5">
                 <img alt="robot" src={`https://robohash.org/${this.props.id}?200x200`} />
                 <div>
                     <h2>{this.props.name}</h2>
