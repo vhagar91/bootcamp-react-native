ef37bf5d24d792996cb38b4cdc6640d98713a3aa
oviera <oviera@vmware.com>
2024-02-19 18:19:11 +0200

    Monsters

--- robot-friends/src/components/Card.jsx
+++ robot-friends/src/components/Card.jsx
@@ -4,7 +4,7 @@ class Card extends React.Component {
     render() {
         return (
             <div className="tc bg-light-purple dib br3 pa3 ma2 grow bw2 shadow5">
-                <img alt="robot" src={`https://robohash.org/${this.props.id}?200x200`} />
+                <img alt="robot" src={`https://robohash.org/${this.props.id}?set=set2&200x200`} />
                 <div>
                     <h2>{this.props.name}</h2>
                     <p>{this.props.email}</p>
