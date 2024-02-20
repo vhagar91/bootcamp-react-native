e62ff0ddcfe9f6331aab4d15638aa2e42112ab9b
oviera <oviera@vmware.com>
2024-02-20 14:26:58 +0200

    Fetch Users from mock server

--- robot-friends/src/App.jsx
+++ robot-friends/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import RoboList from "./components/RobotList";
 import SearchBox from "./components/SearchBox";
-import { robots } from "./database/robots";
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots,
+            robots: [],
             searchFilter: ''
         }
     }
@@ -25,11 +24,16 @@ class App extends React.Component {
 
         return (
             <div className="tc">
-                <h1 > Los Robertos</h1>
+                <h1 className="xbox-logo"> Los Robertos</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <RoboList robots={robotsFiltered} />
             </div>
         )
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
+        .then(users => this.setState({ robots: users }));
     }
 }
 export default App;
