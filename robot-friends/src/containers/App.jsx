b21a1c512c337df12069b011cca49dbcb33996da
oviera <oviera@vmware.com>
2024-02-22 09:42:12 +0200

    Hooks

--- robot-friends/src/containers/App.jsx
+++ robot-friends/src/containers/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
     const [robots, setRobots] = useState([]);
     const [searchFilter, setSearchFilter] = useState('');
+    const [count, setCount] = useState(0);
 
     const onSearchChange = (event) => {
         console.log(event.target.value);
@@ -17,7 +18,7 @@ function App() {
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
             .then(users => setRobots(users));
-    },[])
+    },[count]) // Only changes if count change
 
     const robotsFiltered = robots.filter(robot => {
         return robot.name.includes(searchFilter);
@@ -26,6 +27,7 @@ function App() {
     return (
         <div className="tc">
             <h1 className="xbox-logo"> Los Robertos</h1>
+            <button onClick={()=> setCount(count +1)}>Click Me</button>
             <SearchBox searchChange={onSearchChange} />
             <Scroll>
                 <ErrorBoundry>
